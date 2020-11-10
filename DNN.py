8fefcd526912a760f670bdcefd8d9f0034098f44
HiguTaka <higuchitakayuki.tokushima@gmail.com>
2020-11-10 02:01:10 +0000

    DNN

--- DNN.py
+++ DNN.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 import tensorflow as tf
 import matplotlib.pyplot as plt
 import numpy as np
@@ -9,26 +10,27 @@ from sklearn.metrics import mean_squared_error
 import csv
 
 
-def LoadData(inputfile,outputfile):
-	f1=np.loadtxt(inputfile,delimiter=',')
-	f2=np.loadtxt(outputfile,delimiter=' ')
+#ファイルの読み込み
+def load_data(inputfile,outputfile):
+	f_input=np.loadtxt(inputfile,delimiter=',')
+	f_output=np.loadtxt(outputfile,delimiter=' ')
 
-	CT_values=np.array(f1)
-	Spectrum=np.array(f2)
+	ct_values=np.array(f_input)
+	spectrum=np.array(f_output)
 
-
-	x_train,x_test,y_train,y_test=train_test_split(CT_values,Spectrum,test_size=0.2,random_state=0)
+	x_train,x_test,y_train,y_test=train_test_split(ct_values,spectrum,test_size=0.2,random_state=0)
 
 	return x_train,x_test,y_train,y_test
 
-def Mean_Std(x_train):
+#平均、標準偏差の計算
+def calc_mean_std(x_train):
 	xmean=np.mean(x_train,axis=0)
 	xstd=np.std(x_train,axis=0)
 	
 	return xmean,xstd
 
-
-def Zscore_nomalization(x_train,xmean,xstd):
+#Zスコアによる正規化
+def zscore_nomalization(x_train,xmean,xstd):
 	
 	x_train_norm=np.zeros((x_train.shape[0],x_train.shape[1]))
 
@@ -38,7 +40,8 @@ def Zscore_nomalization(x_train,xmean,xstd):
 
 	return x_train_norm
 
-def MinMax_normalization(x_train,x_test):
+#MinMaxScalerによる正規化
+def minmax_normalization(x_train,x_test):
 	mmsc = MinMaxScaler()
 	x_train_norm = mmsc.fit_transform(x_train)
 	x_test_norm = mmsc.transform(x_test)
@@ -46,7 +49,7 @@ def MinMax_normalization(x_train,x_test):
 
 	return x_train_norm,x_test_norm
 
-
+#モデルの生成
 def model_make():
 	model = tf.keras.Sequential([
     	tf.keras.layers.Dense(14,input_shape=(14,)),
@@ -64,15 +67,16 @@ def model_make():
 	
 	return model
 
-def model_compile_MSE(model):
+
+def model_compile_mse(model):
 	model.compile(optimizer='adam',
 		loss='mean_squared_error')
 
-def model_compile_Crossentropy(model):
+def model_compile_crossentropy(model):
 	model.compile(optimizer='adam',
 		loss='categorical_crossentropy')
 
-def compare_TV(stack):
+def compare_tv(stack):
 	loss = stack.history['loss']
 	val_loss = stack.history['val_loss']
 	epochs = range(len(loss))
@@ -108,27 +112,28 @@ def evaluation(test_predictions,y_test):
 	plt.savefig(filename_min)
 	plt.close()
 
-"""
-#main
-inputfile = './TrainingData/CTnumber7500.csv'
-outputfile = './TrainingData/spectrum7500_normalization.csv'
+def main():
+	inputfile = './TrainingData/CTnumber7500.csv'
+	outputfile = './TrainingData/spectrum7500_normalization.csv'
 
-x_train,x_test,y_train,y_test=LoadData(inputfile,outputfile)
+	x_train,x_test,y_train,y_test=LoadData(inputfile,outputfile)
 
-x_train_norm,x_test_norm=MinMax_normalization(x_train,x_test)
+	x_train_norm,x_test_norm=MinMax_normalization(x_train,x_test)
 
-model = model_make()
+	model = model_make()
 
-model_compile_MSE()
+	model_compile_MSE()
 
-stack=model.fit(x_train_norm,y_train,epochs=1000,batch_size=32,validation_data=(x_test_norm,y_test))
+	stack=model.fit(x_train_norm,y_train,epochs=1000,batch_size=32,validation_data=(x_test_norm,y_test))
 
-compare_TV()
+	compare_TV()
 
-test_predictions = model.predict(x_test_norm)
+	test_predictions = model.predict(x_test_norm)
 
-evaluation(test_predictions,y_test)
+	evaluation(test_predictions,y_test)
 
-model.save('model.h5')
-#np.set_printoptions(threshold=np.inf)
-"""
+	model.save('model.h5')
+	#np.set_printoptions(threshold=np.inf)
+
+if __name__=="__main__":
+	main()
